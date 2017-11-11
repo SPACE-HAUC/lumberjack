c67a3ea09cce7c14b99c178a8a951048357f6b6a
David Connolly <dpc15038@gmail.com>
2017-11-11 16:41:33 -0500

    Added code for making database

--- sqlite_driver.hpp
+++ sqlite_driver.hpp
@@ -47,4 +47,25 @@ int readFromDatabase();
  */
 void closeDatabase();
 
+/*
+** This function is used to load the contents of a database file on disk 
+** into the "main" database of open database connection pInMemory, or
+** to save the current contents of the database opened by pInMemory into
+** a database file on disk. pInMemory is probably an in-memory database, 
+** but this function will also work fine if it is not.
+**
+** Parameter zFilename points to a nul-terminated string containing the
+** name of the database file on disk to load from or save to. If parameter
+** isSave is non-zero, then the contents of the file zFilename are 
+** overwritten with the contents of the database opened by pInMemory. If
+** parameter isSave is zero, then the contents of the database opened by
+** pInMemory are replaced by data loaded from the file zFilename.
+**
+** If the operation is successful, SQLITE_OK is returned. Otherwise, if
+** an error occurs, an SQLite error code is returned.
+*/
+// NEEDS TO BE CALLED AFTER EACH WRITE TO ENSURE DATA IS SAVED
+// #REFACTORME
+int loadOrSaveDb(sqlite3 *pInMemory, const char *zFilename, int isSave);
+
 #endif  // __SQLITE_DRIVER_H
